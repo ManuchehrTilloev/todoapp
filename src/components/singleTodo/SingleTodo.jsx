184d20d6e2c9cdb014f34e1bd025a07dd21faf6b
orshah <orshpov@gmail.com>
2023-05-18 19:22:24 -0500

    first commit

---
+++ src/components/singleTodo/SingleTodo.jsx
@@ -0,0 +1,8 @@
+import "./singleTodo.style.css";
+import React from "react";
+
+function SingleTodo() {
+  return <h1>Single Todo</h1>;
+}
+
+export default SingleTodo;
---
+++ src/components/todoinput/TodoInput.jsx
@@ -0,0 +1,12 @@
+import "./todoInput.style.css";
+import React from "react";
+
+function TodoInput() {
+  return (
+    <div>
+      <h1>Todo input</h1>
+    </div>
+  );
+}
+
+export default TodoInput;
